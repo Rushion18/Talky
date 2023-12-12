2d8a310ed595a3554f43aae6dd8ff7bfb351fb22
Rushion Chegge <136695621+Rushion18@users.noreply.github.com>
2023-12-12 17:28:09 +0300

    Edited User Feed

--- talky-project/src/app/app-routing.module.ts
+++ talky-project/src/app/app-routing.module.ts
@@ -3,11 +3,14 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginAuthenticationComponent } from './login-authentication/login-authentication.component';
 import { RegisterAuthenticationComponent } from './register-authentication/register-authentication.component';
 import { UserPageComponent } from './user-page/user-page.component';
+import { SavedCollectionsComponent } from './saved-collections/saved-collections.component';
+import { HomePageComponent } from './home-page/home-page.component';
 
 const routes: Routes = [
   { path: 'register', component: RegisterAuthenticationComponent },
   { path: 'login', component: LoginAuthenticationComponent },
   { path: 'profile', component: UserPageComponent},
+  { path: 'home', component: HomePageComponent}
 ];
 
 
